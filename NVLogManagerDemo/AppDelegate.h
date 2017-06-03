b648d649cc06e20c4fa47824b3bb9833e1207063
Jackey <zhouxihi@aliyun.com>
2017-06-03 12:38:11 +0800

    Initial Commit

---
+++ NVLogManagerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NVLogManagerDemo
+//
+//  Created by Jackey on 2017/6/3.
+//  Copyright © 2017年 com.zhouxi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NVLogManagerDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NVLogManagerDemo
+//
+//  Created by Jackey on 2017/6/3.
+//  Copyright © 2017年 com.zhouxi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
